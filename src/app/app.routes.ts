43f2f16b88d4dd8bc12fd444a3621626ba41135c
AliKnows <asteteali@gmail.com>
2025-05-21 13:47:58 -0500

    Crea la pagina inicio y lo Enruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+{
+     path: 'inicio',
+    loadComponent: () => import('./inicio/inicio.component').then(m => m.InicioComponent),
+    title: 'Nuestros Servicios'
+
+},
+];
